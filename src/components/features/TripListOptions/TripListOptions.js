845d0c510f616c52e96c80590671af3e650c79ea
Deidrah <dominikastasik93@gmail.com>
2021-06-01 10:59:18 +0200

    fix navigation, links and filters

--- src/components/features/TripListOptions/TripListOptions.js
+++ src/components/features/TripListOptions/TripListOptions.js
@@ -9,15 +9,22 @@ class TripListOptions extends React.Component {
     if(checked) {
       console.log('Adding tag', tag);
       // TODO - use action dispatcher from props
+      this.props.addTag(tag);
     } else {
       console.log('Removing tag', tag);
       // TODO - use action dispatcher from props
+      this.props.removeTag(tag);
     }
   }
 
   handleDuration(type, value){
     console.log('Changing duration', type, value);
     // TODO - use action dispatcher from props
+    if(type === 'from') {
+      this.props.changeDurationTimeFrom(value);
+    } else {
+      this.props.changeDurationTimeTo(value);
+    }
   }
 
   handleSearch(phrase){
@@ -73,6 +80,10 @@ TripListOptions.propTypes = {
   tags: PropTypes.object,
   filters: PropTypes.object,
   changeSearchPhrase: PropTypes.func,
+  changeDurationTimeFrom: PropTypes.func,
+  changeDurationTimeTo: PropTypes.func,
+  removeTag: PropTypes.func,
+  addTag: PropTypes.func,
 };
 
 export default TripListOptions;
