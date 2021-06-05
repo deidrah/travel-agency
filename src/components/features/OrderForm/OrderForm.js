2feaeccdb74eed9c3e148b862b416cc8517b39a1
Deidrah <dominikastasik93@gmail.com>
2021-06-05 16:00:47 +0200

    adding all option components

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -2,9 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {Col, Row} from 'react-flexbox-grid';
 import OrderSummary from '../OrderSummary/OrderSummary.js';
+import pricing from '../../../data/pricing.json';
+import OrderOption from '../OrderOption/OrderOption';
 
 const OrderForm = props => (
   <Row>
+    {pricing.map((option) => 
+      <Col md={4} key={option.id}>
+        <OrderOption {...option} currentValue = {props.options[option.id]} setOrderOption={props.setOrderOption} />
+      </Col>
+    )}
     <Col xs={12}>
       <OrderSummary cost={props.tripCost} options={props.options}/>
     </Col>
@@ -14,6 +21,8 @@ const OrderForm = props => (
 OrderForm.propTypes = {
   tripCost: PropTypes.string,
   options: PropTypes.any,
+  setOrderOption: PropTypes.func,
+  name: PropTypes.string,
 };
 
 export default OrderForm; 
