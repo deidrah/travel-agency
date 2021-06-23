0decc04ebd5bb013b5ed108daea4539a1b265514
Deidrah <dominikastasik93@gmail.com>
2021-06-23 22:38:06 +0200

    Add HappyHourAd component

---
+++ src/components/features/HappyHourAd/HappyHourAd.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import styles from './HappyHourAd.scss';
+import PropTypes from 'prop-types';
+
+class HappyHourAd extends React.Component {
+  constructor() {
+    super();
+    setInterval(() => {
+      this.forceUpdate();
+    }, 1000);
+  }
+  getCountdownTime() {
+    const currentTime = new Date();
+    const nextNoon = new Date(Date.UTC(currentTime.getUTCFullYear(), currentTime.getUTCMonth(), currentTime.getUTCDate(), 12, 0, 0, 0));
+    if (currentTime.getUTCHours() >= 12) {
+      nextNoon.setUTCDate(currentTime.getUTCDate() + 1);
+    }
+    return Math.round((nextNoon.getTime() - currentTime.getTime()) / 1000);
+  }
+  render() {
+    const { title, promoDescription } = this.props;
+    const getCountdownTime = this.getCountdownTime();
+    return (
+      <div className={styles.component}>
+        <h3 className={styles.title}>{title}</h3>
+        <div className={styles.promoDescription}>{this.getCountdownTime()}
+          {getCountdownTime > 23 * 60 * 60 && promoDescription}
+        </div>
+      </div>
+    );
+  }
+}
+
+HappyHourAd.propTypes = {
+  title: PropTypes.string,
+  promoDescription: PropTypes.string,
+};
+
+export default HappyHourAd;
